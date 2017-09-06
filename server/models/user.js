787e42f55ffebd777b18ad25379ca8cd432644ec
Elroy Jetson <ElroyJetson@lees-MBP.home>
2017-09-05 20:13:43 -0400

    created routes

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
 
-return User;
+return user;
 
   user.associate = (models) => {
     user.hasMany(models.questions, {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,25 @@
-const userController = require('../controllers').user;
-const passport = require('passport')
-const api = require('express').Router()
+'use strict'
 
-module.exports = api
-  api.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the user API!',
+const userController = require('../controllers').user;
+const questionsController = require('../controllers').questions;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the User API!',
   }));
 
-  api.post('/api/user/:userId/items', userController.create);
-  api.get('/api/user/:userId', userController.list);
-  api.put('/api/user/:userId', userController.update);
-  api.delete('/api/user/:userId', userController.destroy);
+  app.post('/api/user', userController.create);
+  app.get('/api/user', userController.list);
+  app.get('/api/user/:userId', userController.retrieve);
+  app.put('/api/user/:userId', userController.update);
+  app.delete('/api/user/:userId', userController.destroy);
+
+  app.post('/api/user/:userId/items', questionsController.create);
+  app.put('/api/user/:userId/items/:questionsId', questionsController.update);
+  app.delete(
+    '/api/user/:userId/items/:questionsId', questionsController.destroy
+  );
+  app.all('/api/user/:userId/items', (req, res) => res.status(405).send({
+    message: 'Method Not Allowed',
+  }));
 };
